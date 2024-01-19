59ccca8e0ed523b38c2606e2db671d2c4823759a
Arkadiusz Daugielewicz <akson_control@o2.pl>
2024-01-19 20:13:35 +0100

    fix: sign-up

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,26 +1,73 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  createTheme,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import { Helmet } from "react-helmet";
 import { useDispatch } from "react-redux";
 import { register } from "../../redux/auth/operations";
 import "./Register.module.css";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { NavLink } from "react-router-dom";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const defaultTheme = createTheme();
 
 const Register = () => {
   const dispatch = useDispatch();
-  const handleRegistration = (event) => {
+  // const handleRegistration = (event) => {
+  //   event.preventDefault();
+  //   // const target = event.target;
+  //   // const currentTarget = event.currentTarget;
+  //   const name = event.target.username.value;
+  //   const email = event.target.email.value;
+  //   const password = event.target.password.value;
+  //
+  //   // console.log("register-tareget: ", target);
+  //   // console.log("register-CurrTareget: ", currentTarget);
+  //
+  //   dispatch(register({
+  //     name,
+  //     email,
+  //     password,
+  //   }));
+  // };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    // const target = event.target;
-    // const currentTarget = event.currentTarget;
-    const name = event.target.username.value;
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-
-    // console.log("register-tareget: ", target);
-    // console.log("register-CurrTareget: ", currentTarget);
-
-       dispatch(register({
-        name,
-        email,
-        password,
-      }));
+    const data = new FormData(event.currentTarget);
+    dispatch(register({
+      name: `${data.get("firstName")} ${data.get("lastName")}`,
+      email: data.get("email"),
+      password: data.get("password"),
+    }));
   };
 
   return (
@@ -30,7 +77,7 @@ const Register = () => {
         {/*<link rel="canonical" href="http://" />*/}
       </Helmet>
 
-      <form onSubmit={handleRegistration}>
+      {/*<form onSubmit={handleRegistration}>
         <label>Username</label>
         <input name="username" />
 
@@ -41,7 +88,107 @@ const Register = () => {
         <input name="password" type="password" autoComplete="off" />
 
         <button type="submit">Register</button>
-      </form>
+      </form>*/}
+
+      {/*///////////////////*/}
+
+
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox value="allowExtraEmails" color="primary" />
+                    }
+                    label="I want to receive inspiration, marketing promotions and updates via email."
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <NavLink to="/login"><Link href="#" variant="body2">
+                    Already have an account? Sign in
+                  </Link></NavLink>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 5 }} />
+        </Container>
+      </ThemeProvider>
+
     </div>
   );
 };
